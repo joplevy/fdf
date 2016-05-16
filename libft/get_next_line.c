51f14d56954c75f8db42566188b60eaa3816f92f
joplevy <joey.plevy@gmail.com>
2016-05-16 19:53:02 +0200

    lets go

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,31 +6,28 @@
 /*   By: jplevy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/04 15:41:04 by jplevy            #+#    #+#             */
-/*   Updated: 2016/04/29 01:06:16 by jplevy           ###   ########.fr       */
+/*   Updated: 2016/04/30 04:04:40 by joeyplevy        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#define BUFF_SIZE 32
+#include "get_next_line.h"
+#include <stdio.h>
 
-static int		ft_indexof(char *str)
+int		ft_indexof(char *str)
 {
 	int		i;
 
 	i = 0;
-	while (str[i] && str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] == '\n')
+		if (str[i] == '\n' || str[i] == 26)
 			return (i);
 		i++;
 	}
 	return (-2);
 }
 
-static int		get_chars(char *buff, char **str)
+int		get_chars(char *buff, char **str)
 {
 	char	*tmp;
 
@@ -44,7 +41,7 @@ static int		get_chars(char *buff, char **str)
 	return (ft_indexof(*str));
 }
 
-static int		ft_result(char **line, char **str, int n)
+int		ft_result(char **line, char **str, int n)
 {
 	char	*tmp;
 
@@ -54,36 +51,64 @@ static int		ft_result(char **line, char **str, int n)
 		return (-1);
 	free(*str);
 	*line = ft_strsub(tmp, 0, n);
-	*str = ft_strsub(tmp, n + 1, ft_strlen(tmp) - n);
+	*str = ((n == (int)ft_strlen(tmp)) ? ft_strnew(0) : \
+		ft_strsub(tmp, n + 1, ft_strlen(tmp) - n));
 	free(tmp);
 	return (1);
 }
 
-int				get_next_line(int const fd, char **line)
+t_list_fd	*ft_fd_finder(t_list_fd **mem, int fd)
 {
-	int			i;
-	int			j;
-	static char	*str;
-	char		*buffer;
+	t_list_fd	*tmp1;
+
+	tmp1 = *mem;
+	if (tmp1 == NULL)
+	{
+		if (!(tmp1 = (t_list_fd *)malloc(sizeof(t_list_fd))) ||
+			!(tmp1->line = ft_strnew(0)))
+			return (NULL);
+		tmp1->fd = fd;
+		tmp1->next = NULL;
+		*mem = tmp1;
+	}
+	while (tmp1->fd != fd)
+	{
+		if (tmp1->next == NULL)
+		{
+			if (!(tmp1->next = (t_list_fd *)malloc(sizeof(t_list_fd))) ||\
+			!(tmp1->next->line = ft_strnew(0)))
+				return (NULL);
+			tmp1->next->fd = fd;
+			tmp1->next->next = NULL;
+		}
+		tmp1 = tmp1->next;
+	}
+	return (tmp1);
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	int					i;
+	int					j;
+	static t_list_fd	*mem = NULL;
+	t_list_fd			*tmp;
+	char				*buffer;
 
 	if (fd < 0)
 		return (-1);
 	if (!(buffer = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	if (!(str))
-	{
-		if (!(str = ft_strnew(0)))
-			return (-1);
-	}
-	else if ((j = ft_indexof(str)) != -2)
-		return (ft_result(line, &str, j));
-	i = read(fd, buffer, BUFF_SIZE);
-	if (i == 0 && ft_strlen(str) == 0)
-		return (0);
-	if ((j = get_chars(buffer, &str)) == -1 || i == -1)
+	if (!(tmp = ft_fd_finder(&mem, fd)))
 		return (-1);
+	if ((j = ft_indexof(tmp->line)) != -2)
+		return (ft_result(line, &(tmp->line), j));
+	i = read(fd, buffer, BUFF_SIZE);
+	if ((j = get_chars(buffer, &(tmp->line))) == -1 || i == -1)
+		return (-1);
+	if (i == 0 && ft_strlen(tmp->line) == 0)
+		return (0);
 	if (j == -2 && i != 0)
 		return (get_next_line(fd, line));
 	else
-		return (ft_result(line, &str, j));
+		return (ft_result(line, &(tmp->line), j));
 }
---
+++ libft/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jplevy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/04 10:59:44 by jplevy            #+#    #+#             */
+/*   Updated: 2016/04/30 03:34:04 by joeyplevy        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/time.h>
+# include "libft.h"
+
+typedef struct			s_list_fd
+{
+	int					fd;
+	char				*line;
+	struct s_list_fd	*next;
+}						t_list_fd;
+
+int					ft_indexof(char *str);
+int					get_chars(char *buff, char **str);
+int					ft_result(char **line, char **str, int n);
+int					get_next_line(int const fd, char **line);
+t_list_fd			*ft_fd_finder(t_list_fd **mem, int fd);
+#endif
